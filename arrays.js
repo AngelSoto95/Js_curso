66da9b151eedd2339825625616174469ddfcc33c
Angel Soto <angelsotoo9525@gmail.com>
2024-02-05 01:15:22 -0500

    se practica con las estructuras arrays

--- arrays.js
+++ arrays.js
@@ -1,2 +1,24 @@
 'use strict'
 
+// Arrays - arreglos - matrices 
+
+var nombre = "Angel Soto";
+var nombres = ["angel soto", "Juan Soto", "Yesica Soto", 56 , 76, true];
+
+var lenguajes = new Array("PHP","jS", "Go", "Java", "c#", "Python");
+
+// var elemento = parseInt(prompt("Que elemento del array quieres?", 0));
+// if(elemento >= nombres.length){
+//     alert("Introduce el numero correcto menor que " + nombres.length);
+// }else{
+//     alert("El ususario seleccionado es: " + nombres[elemento]);
+// }
+//alert(nombres[elemento]);
+
+document.write("<h1>Lenguajes de programación del 2018</h1>");
+document.write("<ul>");
+for(var i = 0; i < lenguajes.length; i++){
+    document.write("<li>" + lenguajes[i] + "</li>");
+}
+
+document.write("</ul>");
